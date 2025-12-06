4df46615ec764693d815adc06767c4ea80056b78
bilolbek <komilovasodatjon@gmail.com>
2025-12-06 15:48:37 +0300

    hi firstname

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-import "./src/bot/bot.js"
 
-console.log("dastur boshlanmoqda...");
+import "./src/bot/bot.js";
+import mongoose from "mongoose";
+import { config } from "dotenv";
+config();
+
+// Db connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log(`Db is connected...`);
+  })
+  .catch(() => {
+    console.log(`Error: db is not connected!`);
+  });
+
+console.log("Dastur boshlanmoqda...");
--- src/bot/bot.js
+++ src/bot/bot.js
@@ -1,1 +1,29 @@
-console.log("bot ishga tushdi...");
+import TelegramBot from "node-telegram-bot-api";
+import { config } from "dotenv";
+import onStart from "./handlers/onStart.js";
+import onProfile from "./handlers/onProfile.js";
+import onError from "./handlers/onError.js";
+config();
+
+const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  const firstname = msg.chat.first_name;
+  const text = msg.text;
+
+  bot.sendMessage(chatId, `Assalomu aleykum, ${firstname}`);
+  bot.sendMessage(chatId, `${text}`);
+  if (text == "/start") {
+    return onStart(msg);
+  }
+
+  if (text == "/profile") {
+    return onProfile(msg);
+  }
+
+  return onError(msg);
+});
+
+console.log("Bot ishga tushdi...");
+
